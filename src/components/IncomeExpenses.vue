d4e2b4e17ff9d134065cb0290a01b2fb872e00ea
ShahadAlkamli <85537889+ShahadAlkamli@users.noreply.github.com>
2026-09-24 18:15:43 +0300

    Update IncomeExpenses.vue

--- src/components/IncomeExpenses.vue
+++ src/components/IncomeExpenses.vue
@@ -2,19 +2,17 @@
   <div class="inc-exp-container">
     <div>
       <h4>Income</h4>
-      <p id="money-plus" class="money plus">+${{ income }}</p>
+      <p id="money-plus" class="money plus">+${{ income.toFixed(2) }}</p>
     </div>
     <div>
       <h4>Expense</h4>
-      <p id="money-minus" class="money minus">-${{ expenses }}</p>
+      <p id="money-minus" class="money minus">-${{ Math.abs(expenses).toFixed(2) }}</p>
     </div>
   </div>
 </template>
 
 <script setup>
-import { defineProps } from 'vue';
-
-const props = defineProps({
+defineProps({
   income: {
     type: Number,
     required: true,
